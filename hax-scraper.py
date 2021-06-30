a633476b22c9c9f847066299cbd2a8bb675c1de3
Ty Arpornsuksant <tyarpornsuksant@10-16-20-111.dynapool.wireless.nyu.edu>
2021-06-30 13:06:38 -0400

    added git ignore and other stuff, also renamed main to hax-scraper

--- hax-scraper.py
+++ hax-scraper.py
@@ -1,14 +1,5 @@
-# Filters no accel https://www.techstars.com/portfolio?industries=Artificial+Intelligence+%26+Machine+Learning
-# &industries=Biotech&industries=Blockchain+%26+Cryptocurrency&industries=Cleantech&industries=Cybersecurity
-# &industries=Developer+Tools&industries=Fintech&industries=Future+of+Work&industries=Gaming&industries=Govtech+%26
-# +Regtech&industries=Health+%26+Wellness&industries=Healthcare+%28hardware%29&industries=Healthcare+%28software%29
-# &industries=Life+Sciences&industries=Pharma&industries=Robotics&industries=SaaS&industries=Smart+Cities&industries
-# =Technology&industries=VR+%26+AR&status=Operating
 
-
-import requests
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
 import time
 import csv
@@ -36,12 +27,6 @@ driver_path = "/Users/tyarpornsuksant/Downloads/chromedriver"
 driver = webdriver.Chrome(driver_path)
 
 
-# directory_url = "https://sosv.com/portfolio/?program=HAX&stage=Accelerator%2CPre-seed%2CSeed&page=" + str(1)
-# driver.get(directory_url)
-# # time.sleep(3)  # for page to load
-# source = driver.page_source
-# soup = BeautifulSoup(source, 'html.parser')
-# print(soup)
 names = []
 founders = []
 linkedins = []
@@ -53,7 +38,8 @@ company_hrefs = []
 page_number = 1
 max_pages = 6
 while page_number <= max_pages:
-    directory_url = "https://sosv.com/portfolio/?program=HAX&stage=Accelerator%2CPre-seed%2CSeed&page=" + str(page_number)
+    # directory_url = "https://sosv.com/portfolio/?program=HAX&stage=Accelerator%2CPre-seed%2CSeed&page=" + str(page_number)
+    directory_url = "https://sosv.com/portfolio/?program=HAX&stage=Program%2CPre-seed%2CSeed&page=" + str(page_number)
     driver.get(directory_url)
     time.sleep(3)  # for page to load
     source = driver.page_source
@@ -64,19 +50,30 @@ while page_number <= max_pages:
     location_raw = soup.find_all('footer', class_="sosv-location-text")
     hrefs = soup.find_all('a', class_="card-body p-3 d-flex flex-column justify-content-between")
     for t in titles_raw:
-        names.append(t.text)
+        try:
+            names.append(t.text)
+        except:
+            print("name for " + t + " not found")
+            names.append(" ")
     for d in descriptions_raw:
-        descriptions.append(d.text)
+        try:
+            descriptions.append(d.text)
+        except:
+            print("description for " + d + " not found" )
+            descriptions.append(" ")
     for l in location_raw:
-        locations.append(l.text)
+        try:
+            locations.append(l.text)
+        except:
+            print("location for " + l + " not found")
+            locations.append(" ")
     for h in hrefs:
-        company_hrefs.append(h['href'])
+        try:
+            company_hrefs.append(h['href'])
+        except:
+            print("href not found")
+            company_hrefs.append("xxxxx")
     page_number += 1
-
-# print(names)
-# print(descriptions)
-# print(locations)
-# print(company_hrefs)
 
 iso_url = "https://sosv.com"
 for link in company_hrefs:
@@ -87,9 +84,16 @@ for link in company_hrefs:
     try:
         websites_links_raw = iso_soup.find_all('a', class_="sosv-link-dark mb-3")
         weblinks = []
+        wn = 1
         for w in websites_links_raw:
             if w['href'][0] == "h":
                 websites.append(w['href'])
+                break
+            else:
+                wn += 1
+        if wn == len(websites_links_raw) + 1:
+            print("website not found")
+            websites.append(" ")
     except:
         print("website for " + link + " not found")
         websites.append(" ")
@@ -111,24 +115,27 @@ for link in company_hrefs:
         linkedins.append(" ")
 
 
-print(names)
-print(founders)
-print(linkedins)
-print(websites)
-print(descriptions)
-print(locations)
+print(str(len(names)) + "names")
+print(str(len(founders)) +"founders")
+print(str(len(linkedins)) +"linkedins")
+print(str(len(websites)) +"websites")
+print(str(len(descriptions)) +"descriptions")
+print(str(len(locations)) +"locations")
 
+print(names)
+print(websites)
 
 companies = []
 for r in range(len(names)):
-    companies.append(Company(names[r], founders[r], linkedins[r], websites[r],descriptions[r], locations[r]))
+    companies.append(Company(names[r], founders[r], linkedins[r], websites[r], descriptions[r], locations[r]))
 
 with open('hoxinfo.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["title", "location", "founder", "description", "linkedin", "website"])
+    writer.writerow(["title", "founders", "linkedin", "website", "description", "location"])
     for company in companies:
         try:
             writer.writerow([company.name, company.founder, company.linkedin, company.website, company.description, company.location])
             print(company.name + " was recorded")
         except:
             print("There was something wrong with " + company.name + ". It could not be recorded")
+
